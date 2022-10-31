8e962b9e8bc26055635f0568fcc52de6aec4be51
karavanets <dront22rus@mail.ru>
2022-10-31 22:13:17 +0700

    add file creator '4_result.json'

--- main.py
+++ main.py
@@ -136,6 +136,9 @@ def get_result():
             item['item_bonus'] = prices.get('item_bonus')
             item['item_link'] = f'https://www.mvideo.ru/products/{item.get("nameTranslit")}-{product_id}'
 
+    with open('data/4_result.json', 'w', encoding='utf-8') as file:
+        json.dump(products_data, file, indent=4, ensure_ascii=False)
+
 
 def main():
     get_data()
